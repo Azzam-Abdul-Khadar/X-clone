60a304092c9851478427aace6ed68d5599d80d87
Azzam Abdul Khadar <azzamcse@gmail.com>
2025-09-16 23:46:36 +0530

    X clone using Tailwind CSS.
    I have created a clone of twitter UI using HTML and Tailwind CSS.

---
+++ Twitter Clone/tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.html"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
